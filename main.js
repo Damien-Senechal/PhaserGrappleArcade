6d10a85487ce1cd71b003ffbf1a0fabc1346b6bf
Damien-Senechal <60384944+Damien-Senechal@users.noreply.github.com>
2022-02-01 18:48:14 +0100

    basic physics

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ var config = {
         arcade: {
             gravity: { y: 200 },
             debug : true,
-            fps : 30,
+            fps : 60,
             terrainObjects: 20,     // amount of terrain objects
             heroSize: 20,           // hero size
             constraintSpeed: 2,     // constraint shrinkage speed
@@ -35,6 +35,7 @@ function preload ()
 }
 
 function create () {
+    this.physics.world.setBounds(10, 10, game.config.width - 20, game.config.height - 20,);
 
     this.hero = this.add.rectangle(game.config.width / 2, game.config.height / 2, 20, 20, 0x6666ff);
     this.physics.add.existing(this.hero);
@@ -63,16 +64,15 @@ function create () {
 
     this.input.on("pointerdown", fireHook, this);
 
-    this.physics.world.on('collide', function(b1, b2){
-
+    this.physics.world.on('collide', function(b1, b2) {
+        ///console.log("Hello")
         // when the ball collides with something, we'll remove the hook
-        if(b1.label === BALL || b2.label === BALL){
+        if (b1.label === BALL || b2.label === BALL) {
             console.log("Hello")
             //this.releaseHook();
         }
 
-        // when the hook collides with something, let's make it static and create the joint
-        /*if((b1.label == HOOK || b2.label == HOOK) &amp;&amp; !this.rope){
+        if ((b1.label === HOOK || b2.label === HOOK) && !this.rope) {
 
             // make the hook static
             Phaser.Physics.Matter.Matter.Body.setStatic(this.hook, true);
@@ -81,18 +81,19 @@ function create () {
             let distance = Phaser.Math.Distance.Between(this.hero.position.x, this.hero.position.y, this.hook.position.x, this.hook.position.y);
 
             // is the distance fairly greater than hero size?
-            if(distance > gameOptions.heroSize * 2){
+            if (distance > gameOptions.heroSize * 2) {
 
                 // add the constraint
-                this.rope = this.matter.add.constraint(this.hero, this.hook, distance, 0);
+                this.rope = this.physics.add.constraint(this.hero, this.hook, distance, 0);
             }
 
-        }*/
+        }
     }, this)
 }
 
-function fireHook(){
-    console.log("Fire !");
+function fireHook(e){
+    let angle = Phaser.Math.Angle.Between(this.hero.body.x, this.hero.body.y, e.position.x, e.position.y);
+    this.physics.add.constraint(this.hero, this.hook, 0, 0);
 }
 
 function update(){
