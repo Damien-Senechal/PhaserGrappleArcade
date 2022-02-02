b51d09d4d608c6fc599e08f982d52e50c089f2ce
Damien-Senechal <60384944+Damien-Senechal@users.noreply.github.com>
2022-02-02 14:14:01 +0100

    try stuff

--- src/Scene.js
+++ src/Scene.js
@@ -24,7 +24,7 @@ class Scene extends Phaser.Scene {
         this.hero.label = this.BALL;
         this.hero.body.onCollide = true
 
-        for (let i = 0; i <= 15; i++) {
+        /*for (let i = 0; i <= 15; i++) {
             let posX = Phaser.Math.Between(0, game.config.width);
             let posY = Phaser.Math.Between(0, game.config.height);
             let width = Phaser.Math.Between(50, 200);
@@ -32,12 +32,22 @@ class Scene extends Phaser.Scene {
             let rect = this.add.rectangle(posX, posY, width, height, 0x6666ff, {isStatic: true});
             rect.label = this.WALL;
             this.boxes.add(rect)
-        }
+        }*/
+        let rect = this.add.rectangle(800, 800, 800, 100, 0x6666ff, {isStatic: true});
+        rect.label = this.WALL;
+        this.boxes.add(rect)
+
+        let rect2 = this.add.rectangle(0, 0, 300, 100, 0x6666ff, {isStatic: true});
+        rect2.label = this.WALL;
+        this.boxes.add(rect2)
+
         this.physics.add.collider(this.hero, this.boxes)
 
         this.test = "NIQUE TA GROSSE MERE"
         this.hook = null;
         this.rope = null;
+        this.distance2 = 0;
+        this.grappling = false;
 
         this.input.on("pointerdown", this.fireHook, this);
         let me = this;
@@ -45,6 +55,14 @@ class Scene extends Phaser.Scene {
 
             if (b1.height === 20 || b2.height === 20) {
                 console.log("Hello")
+                /*if(this.reach){
+                    me.hero.body.setVelocity(0)
+                    me.hero.body.setImmovable(true)
+                    me.hero.body.setAllowGravity(false)
+                }else{
+                    me.hero.body.setImmovable(false)
+                    me.hero.body.setAllowGravity(true)
+                }*/
             }
 
             if (b1.height === 10 || b2.height === 10) {
@@ -54,43 +72,63 @@ class Scene extends Phaser.Scene {
                 me.hook.body.setAllowGravity(false)
 
                 // calculate the distance between the ball and the hook
-                let distance = Phaser.Math.Distance.Between(this.hero.body.x, this.hero.body.y, this.hook.body.x, this.hook.body.y);
+                this.distance = Phaser.Math.Distance.Between(this.hero.body.x, this.hero.body.y, this.hook.body.x, this.hook.body.y);
                 //console.log(distance)
                 let target = new Phaser.Math.Vector2();
                 target.x = this.hook.body.x
                 target.y = this.hook.body.y
 
+                this.grappling = true;
+                this.physics.moveToObject(this.hero, target, 400);
+
                 // is the distance fairly greater than hero size?
-                if(distance > 20 * 2){
+                /*if(distance < 20){
 
                     this.physics.moveToObject(this.hero, target, 400);
+                    this.reach = true
                     // add the constraint
                     //this.rope.matter.add.constraint(this.hero, this.hook, distance, 0);
                     //this.rope = Phaser.Math.Distance.Between(this.hero.body.x, this.hero.body.y, this.hook.body.x, this.hook.body.y) < distance
-                }
+                }*/
             }
 
         }, this)
     }
 
     fireHook(e) {
-        this.releaseHook()
-        let angle = Phaser.Math.Angle.Between(this.hero.body.x, this.hero.body.y, e.position.x, e.position.y);
-        this.hook = this.add.rectangle(this.hero.body.x + (20 * 2) * Math.cos(angle), this.hero.body.y + (20 * 2) * Math.sin(angle), 10, 10);
-        this.physics.add.existing(this.hook);
-        this.hook.body.setVelocity(400 * Math.cos(angle), 400 * Math.sin(angle))
-        this.hook.label = this.HOOK;
-        this.physics.add.collider(this.hook, this.boxes)
-        this.hook.body.onCollide = true;
+            this.releaseHook()
+            let angle = Phaser.Math.Angle.Between(this.hero.body.x, this.hero.body.y, e.position.x, e.position.y);
+            this.hook = this.add.rectangle(this.hero.body.x + (20 * 2) * Math.cos(angle), this.hero.body.y + (20 * 2) * Math.sin(angle), 10, 10);
+            this.physics.add.existing(this.hook);
+            this.hook.body.setVelocity(400 * Math.cos(angle), 400 * Math.sin(angle))
+            this.hook.label = this.HOOK;
+            this.physics.add.collider(this.hook, this.boxes)
+            this.hook.body.onCollide = true;
     }
 
     releaseHook() {
         if(this.hook != null){
             this.hook.destroy();
+            this.reach = false
         }
     }
 
     update() {
+        if(this.hook != null){
+            this.distance2 = Phaser.Math.Distance.Between(this.hero.body.x, this.hero.body.y, this.hook.body.x, this.hook.body.y);
+        }
+        else{
+            this.distance2 = 0;
+        }
 
+        if(this.distance2 < 100 && this.grappling){
+            /*this.hero.body.setVelocityY(0)
+            this.hero.body.setVelocityX(0)
+            if(this.hero.body.x === this.hook.body.x){
+                this.hero.body.setAllowGravity(0)
+            }*/
+            this.grappling = false
+        }
+        console.log(this.distance2);
     }
 }
